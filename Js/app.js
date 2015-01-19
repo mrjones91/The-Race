315f70a9f46d89bd313b668520eaf3157fbef1e6
dijdev <danieljonesdev@gmail.com>
2015-01-19 02:00:25 -0500

    Floors working but not at proper intervals

--- Js/app.js
+++ Js/app.js
@@ -4,14 +4,17 @@ var game = new Core(630, 360); // game stage
     game.preload('./content/chara1.png', './content/testBkg.png', './content/test.png', './content/Block.png'); // preload image
     
     game.onload = function(){
+        game.keybind(66, 'b');
 
+        var lastWidth = 0;
+        var lastX = 0;
         // Define movement constants
         var gravity = 0.5;
         var nextBlock = 50;
         var nextBlockCounter = 0;
 
         //Create Background
-    	var background = new Sprite(630, 360);
+        var background = new Sprite(630, 360);
         background.image = game.assets['./content/testBkg.png'];
         game.rootScene.addChild(background);
         var sizeX = 0;
@@ -44,60 +47,53 @@ var game = new Core(630, 360); // game stage
 
         game.rootScene.addChild(plyr);
 
+        plyr.addEventListener(Event.ENTER_FRAME, function(){
+            if (plyr.y > 400)
+                alert('failed at ' + ticksToFail);
+        });
+
         var floors = [4];
         var fNum = 0;
-        
-        //var newFloor;
-        //Create Ground + new
-        var floor = enchant.Class.create(enchant.Sprite, function(sizeX, sizeY){
-            if (sizeX == 0 || sizeY == 0)
-            {
-                //this = new Sprite(480, 198);
-                enchant.Sprite.call(this, 480, 198);
-                this.x = 30;
-                this.y = 300;
-                this.image = game.assets['./content/Block.png'];
-            }
-            else
-            {
-                enchant.Sprite.call(this, Math.floor( (Math.random() * 400) + 100) , 
-                    Math.floor( (Math.random() * 200) + 20) );
-                this.image = game.assets['./content/Block.png'];
-                this.x = 500;// Math.floor( Math.random() * 100 ) + 50;
-                this.y = game.height - this.height;
-            }
-
-            this.id = fNum;
-            this.image = game.assets['./content/Block.png'];
-                console.log(this.log);
-            this.top = this.y - this.height;
-
-            nextBlock = this.width / 10;
-            //this.addEventListener(Event.ENTER_FRAME, addFloors(this));
-            //console.log(nextBlock);
-//            this.addEventListener(Event.ENTER_FRAME, function(){});
-        }); 
 
         Floor = Class.create(Sprite, {
             initialize: function(x,y){
                 if (x == 0 || y == 0)
                 {
-                    enchant.Sprite.call(this, 480, 198);
+                    enchant.Sprite.call(this, 1500, 198);
+                    this.x = 30;
+                    this.y = 300;
+                    nextBlock = 50;
                 }
                 else
                 {
-                    enchant.Sprite.call(this, x, y);
+                    //enchant.Sprite.call(this, x, y);
+                    enchant.Sprite.call(this, Math.floor( (Math.random() * 500) + 100) , 
+                    Math.floor( (Math.random() * 300) + 150) );
+                    //console.log(floors[fNum - 1].x);
+                    
+                    this.x = (floors[fNum].x + floors[fNum].width) + 100;//(Math.sqrt( Math.pow( floors[fNum].x, 2) + Math.pow( ,2) ));//Math.floor( Math.random() * 100 );// + lastX;
+
+                    this.y = 360 - (this.height / 2);//Math.floor( Math.random() * 360 ) ;
+                    nextBlock = (this.width / 3.75);
+                    //alert(nextBlock);
+                    //console.log("X Pos: " + this.x);
+                    //console.log("Height: " + this.height);
+                    //console.log("Y pos: " + this.y);
                 }
+                this.id = -1;
+                lastWidth = this.width;
+                lastX = this.x;
+
                 this.image = game.assets['./content/Block.png'];
-                this.x = 30;
-                this.y = 300;
                 this.addEventListener(Event.ENTER_FRAME, this.die);
                 this.addEventListener(Event.ENTER_FRAME, this.move);
             },
             die: function(){
-                if (this.x <= (0 - floors[fNum].width))
+                if (this.x <= (0 - this.width))
                 {
+                    lastDead = this;
                     game.rootScene.removeChild(this);
+                    console.log(this.id + ' is dead');
                 }
             },
             move: function(){
@@ -109,88 +105,30 @@ var game = new Core(630, 360); // game stage
             fNum++;
             if (fNum > 4)
                 fNum = 0;
+            idkfloor.id = fNum;
+            //
             floors[fNum] = idkfloor;
-            console.log(idkfloor);
+            //console.log(idkfloor);
                 //console.log('frame');
-                if (floors[fNum].x <= (0)) // - floors[fNum].width))
-                {
-                    game.rootScene.removeChild(floors[fNum]);
-                }
         }
-
-        /*
-        floor.prototype.addEventListener(Event.ENTER_FRAME, function(){
-            this.x -= 5;
-            if (this.x <= (0 - this.width))
-            {
-                game.rootScene.removeChild(this);
-            }
-        });
-*/
-/*
-for (fNum = 0; fNum <= 4; fNum++)
-        {
-            floors[fNum].addEventListener(Event.ENTER_FRAME, function() {
-                floors[fNum].x -= 5;
-                if (floors[fNum].x <= (0 - floors[fNum].width) )
-                    {
-                        floors[fNum].removeEventListener(Event.ENTER_FRAME);
-                        game.rootScene.removeChild(floors[fNum]);
-                        //console.log(fNum + ' removed');
-                        //newFloor.remove();
-                    }
-            });
-        }
-        */
-
-        var makeFloor = function(sizeX, sizeY) {
-            if (sizeX == 0 || sizeY == 0)
-            {
-                var newFloor = new Sprite(480, 198);
-                newFloor.x = 30;
-                newFloor.y = 300;
-            }
-            else
-            {
-                var newFloor = new Sprite( Math.floor( (Math.random() * 400) + 100) , 
-                    Math.floor( (Math.random() * 200) + 20) );
-                newFloor.x = 500;// Math.floor( Math.random() * 100 ) + 50;
-                newFloor.y = game.height - newFloor.height;
-            }
-            
-            newFloor.id = fNum;
-            newFloor.image = game.assets['./content/Block.png'];
-            newFloor.top = newFloor.y - newFloor.height;
-
-            nextBlock = newFloor.width / 10;
-            //console.log(nextBlock);
-
-            fNum++;
-            if (fNum > 4)
-                fNum = 0;
-            floors[fNum] = newFloor;
-            console.log(fNum);
-            floors[fNum].addEventListener(Event.ENTER_FRAME, function() {
-                //console.log('frame');
-                
-            });
-            //alert(newFloor.top);
-            //console.log(floors[fNum].top);
-
-        };
         
+        var ticksToFail = 0;
+        var lastDead = Floor.call(0,0);
+        var checkNew;
         //makeFloor(0, 0);
-        var addFloor = new Floor(0, 0);
-        //addFloor.image = game.assets['./content/Block.png'];
-        //addFloor.x = 5;
-        //addFloor.y = 100;
-        console.log(addFloor.image);
-        addFloors(addFloor);
-
-        console.log(addFloor instanceof floor);
-        console.log(addFloor instanceof Sprite);
-
+        var floor1 = new Floor(0, 0);
+        /*var floor2 = new Floor(0, 0);
+        var floor3 = new Floor(0, 0);
+        var floor4 = new Floor(0, 0);
+        var floor5 = new Floor(0, 0);*/
+        lastDead.id = floor1.id;
+        addFloors(floor1);
         game.rootScene.addChild(floors[fNum]);
+        //console.log(addFloor.width);
+        //console.log(addFloor instanceof floor);
+        //console.log(addFloor instanceof Sprite);
+
+        
 
         //Game Inputs
 		game.rootScene.addEventListener(Event.TOUCH_START, function(){
@@ -206,19 +144,20 @@ for (fNum = 0; fNum <= 4; fNum++)
             if (plyr.velocityY < -7.0)
                 plyr.velocityY = -7.0;
         });
-
+        game.rootScene.addEventListener(Event.B_BUTTON_DOWN, function(){
+            alert();//floors[fNum].die();
+        });
 
         
         //Player Gravity Updates
         plyr.addEventListener(Event.ENTER_FRAME, function(){
             plyr.velocityY += gravity;
             plyr.y += plyr.velocityY;
-
+            ticksToFail++;
                 grounded(floors);
                 if(plyr.onGround == true)
                 {
                     //console.log('Event: ' + plyr.onGround);
-                    //alert(floor.y);
                     for(var i = 0; i <= 4; i++)
                     {
                         if (plyr.intersect(floors[i]))
@@ -228,35 +167,50 @@ for (fNum = 0; fNum <= 4; fNum++)
                             plyr.onGround = true;
                         }
                     }
-                    /*
-                    
-                    plyr.y = newFloor.y - plyr.height;
-                    plyr.velocityY = 0.0;
-                    plyr.onGround = true;
-                    */
                 }
 
-            //if(positionX < 10 || positionX > 190)
-              //  velocityX *= -1;
             });
-        game.rootScene.addEventListener(Event.ENTER_FRAME, function() {
 
+        game.rootScene.addEventListener(Event.ENTER_FRAME, function() {
+            //
+            /*
+                Countdown time until next block created. Calculate if 
+            */
+            //
             nextBlockCounter++;
-            //console.log(game.fps);
             if (nextBlockCounter >= nextBlock)
                 {
                     //makeFloor();
 
-                    var addedFloor = new Floor();
-                  //  addedFloor.x = 5;
-                  //  addedFloor.y = 105;
-                  //  addedFloor.image = game.assets['./content/Block.png'];
-                    addFloors(addedFloor);
+                   
+                    {
+                        
+                        checkNew = fNum >= 2 ? fNum - 2 : fNum + 3;
+                       if (lastDead.id > -1)
+                        {
 
-                    console.log(addedFloor.image);
-                    game.rootScene.addChild(floors[fNum]);//floors[fNum]);
-                    //console.log(nextBlockCounter);
-                    nextBlockCounter = 0;
+
+                        }
+                       // if (checkNew < 0)
+                         //   checkNew = 4;
+                        console.log("Check var " + checkNew);
+                        console.log("lastDead.id " + lastDead.id);
+                        if ( fNum != checkNew)
+                        {
+                            //if (lastDead.id != checkNew)
+                            {
+                            console.log("Floor #" + fNum + " added!");
+                        var addedFloor = new Floor(1, 1);
+                      
+                        //console.log(fNum);
+                        addFloors(addedFloor);   
+                        nextBlockCounter = 0;
+                        game.rootScene.addChild(floors[fNum]);//floors[fNum]);
+                            }
+                        }
+                        else
+                            nextBlockCounter -= 2; 
+                    }
                 }
 
 
